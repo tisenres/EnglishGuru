1a18389a61917468e42c5efa760c7eca4a6e5db4
tisenres <nastywushu@gmail.com>
2023-11-23 23:12:15 +0500

    Add fonts

--- app/src/main/java/com/example/englishguru/app/features/vocabularySections/SectionsAdapter.kt
+++ app/src/main/java/com/example/englishguru/app/features/vocabularySections/SectionsAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.englishguru.app.features.vocabularySections
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
