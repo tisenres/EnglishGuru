34e6aede1f9a5ec417bb75ba7bf93c53ed6bcd43
tisenres <tisenres@gmail.com>
2023-11-30 16:59:40 +0500

    Init chatbot

--- app/src/main/java/com/example/englishguru/app/features/chatbot/WordChatFragment.kt
+++ app/src/main/java/com/example/englishguru/app/features/chatbot/WordChatFragment.kt
@@ -14,12 +14,39 @@ import ai.api.model.AIResponse
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import com.example.englishguru.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-const val KEY = "7b69e2c78edcd78c31b391afdcd2750dde1f7d6c"
+const val KEY = "\n" +
+        "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDJ9ybQ2AxaYdT2\n" +
+        "GjylgUiV11uzMkgl/BjMPt4csk+DCVXV08jfJ4F7sMnib84em4Q6Lhb3ilReESiq\n" +
+        "F/fCB/z8ilCeh+TzemSVQ10thayONGKCWHVe9S9y4wTOQFLC1sSFzXsvE/2/xS8f\n" +
+        "dwNhjII+v4S8kAjJYKNVkWZ97dEWoa6q+xjZfJ+m+Jcvwz06MdCiNWJJLZ6xvrj4\n" +
+        "G7xlMiPbTNsvMX9J4349U9GAX/eVNayYF+Y4Q+IzsnK6vHPZVKYVXuyZwYZO7ToK\n" +
+        "iDSjkxQk6yZkXPPWFs9j0+X+lLl99FKa8sIcplKbxRn7NcN2EOXX0dF2vikSWEyr\n" +
+        "WdSWc3j9AgMBAAECggEADlKuiEeWIq/G+lgUfnlLdEL0YCI7/yOgf5Wiy16y6FxY\n" +
+        "X8R3/alv052PR7V9pOqLB1Q+8Tm4b35gh6WQ/svxu1g0zPVevYyrxvEi0chl2yTo\n" +
+        "ao2K71vMjGWAsP7bT7GK+OFAb++HQpshkyXt0bR1BS2wZG4DjYKCut4Hqn0b47Kd\n" +
+        "vtZo5+YiOJJ+0SMe++tArrnzZWuQoyYpYE/DLp8eCfwA1SocY4b3T8dxgZjf8WB0\n" +
+        "AYSH1e/PESOrl48cer8ip/bL50ylf1KTDWMdx+Lf+K4xw3TwBUcJUOKiHPnmAmq9\n" +
+        "8FOZj9uuPirOTyFda9YQqHO/4zuFoKqGL8UtyWNLCQKBgQD9jOdL+AjfLHcG94Fz\n" +
+        "L2gUrpDKLNwJxJUzPkg2QjI19TKA309QNOQKjB+1MinefA6Ho/UNdPhs8ICzyVGB\n" +
+        "sCwYxQqCXOeYq5Y1yEN8U1es6eRvZuCELB2dHEBCtJmCVqDpX0ltFVl+bI9YZQln\n" +
+        "jyPexZ/JvO64GTOh7lKVFENOZQKBgQDL6qo9Mz6Qq9GWaLQhJk0qC3xEwOauZzCu\n" +
+        "chAHuEPSIfkYIzBaloOAmFWgA6qh2VybOgf5vnskyvffA8agCzA2qrSex3VZTuXO\n" +
+        "RLXRf7BjW64GT7lNIWX9JSP2XyxAZ00484gR2LPWYdqO3/UqYzAkt5fecax5Es1m\n" +
+        "b6zfK1JquQKBgEabGEXVelL7hOHRxqCW0PsBaZyDRXsK3Sl57I3/fQ/9ieTNSr7h\n" +
+        "G6e8QZApufMUxM8M306yBgQALTbjlujwcDctl7GZwXyIO7oaXICgjMfAEjbbhSYJ\n" +
+        "Su3XUiXBOQZMrV6PhFj2GwfTn09UQ3wtJiYBDnAVRrzJd9FeFm0hSWTpAoGABE80\n" +
+        "Q9DWbKZE6qHR1LWDgfT2LlnJRjxGChm8/vzE6ibvpbPwrKlrpAcUICqTq5H1ZA5o\n" +
+        "5r1AyvlDHcJtqyoxXaZj5MsgTr/dGWvYT9Ib/y2L0vRSXS/sgNoNFYLTY5ULS73R\n" +
+        "zVbADjLJNKXE/51TAiOjIP9T/scH8jc/9wNqXXkCgYAqLQa45nHqDYhK3JS3hxoH\n" +
+        "UEgFtwnokexgKmGj2+VoWVZd8294XDOYoSilp6uk0+RahcS60Xb9eUm7tq6GcOUQ\n" +
+        "jTy8mN9QNErE3wq1TwzzVzwnQLQMkObthFagB2oKUYvdIsOLcwUOCLrid9vfZpQa\n" +
+        "klWJ+DP//+O18g9WRbasNQ==\n"
 
 class WordChatFragment : Fragment() {
 
@@ -53,9 +80,15 @@ class WordChatFragment : Fragment() {
             binding.messageInput.text.clear()
         }
 
-        val config = AIConfiguration(KEY,
+//        val config = AIConfiguration(KEY,
+//            ai.api.AIConfiguration.SupportedLanguages.English,
+//            AIConfiguration.RecognitionEngine.System)
+
+        val config = AIConfiguration(
+            KEY,
             ai.api.AIConfiguration.SupportedLanguages.English,
-            AIConfiguration.RecognitionEngine.System)
+            AIConfiguration.RecognitionEngine.System
+        )
 
         aiDataService = AIDataService(requireContext(), config)
     }
@@ -66,39 +99,26 @@ class WordChatFragment : Fragment() {
     }
 
     private fun sendRequest(query: String) {
-        val request = AIRequest()
-        request.setQuery(query)
+        if (::aiDataService.isInitialized) {
+            Thread {
+                try {
+                    val request = AIRequest()
+                    request.setQuery(query)
 
-//        CoroutineScope(Dispatchers.IO).launch {
-//            try {
-//                val response: AIResponse = aiDataService.request(request)
-//                val botMessage = response.result.fulfillment.speech
-//
-//                withContext(Dispatchers.Main) {
-//                    sendMessage(Message(botMessage, false))
-//                    Log.d("SDSDSDSDSDSDSDSDSD", botMessage.toString())
-//                }
-//            } catch (e: Exception) {
-//                e.printStackTrace()
-//            }
-//        }
+                    val response = aiDataService.request(request)
+                    val botMessage = response.result.fulfillment.speech
 
-        var response: AIResponse? = null
-
-        Thread {
-            try {
-                response = aiDataService.request(request)
-                val botMessage = response.result.fulfillment.speech
-
-                Handler(Looper.getMainLooper()).post {
-                    sendMessage(Message(botMessage, false))
+                    Handler(Looper.getMainLooper()).post {
+                        sendMessage(Message(botMessage ?: "", false))
+                        Log.d("WordChatFragment", "Response: ${response.result.toString()}")
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    Log.e("WordChatFragment", "Error during API request: ${e.message}")
                 }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }.start()
-
-        Log.d("SDJHSHJDHJSDsd", response?.result.toString())
-
+            }.start()
+        } else {
+            Log.e("WordChatFragment", "aiDataService is not initialized")
+        }
     }
 }
