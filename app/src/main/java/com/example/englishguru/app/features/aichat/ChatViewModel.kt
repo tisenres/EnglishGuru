9874eebbda196923c788a7e22f323811a7265377
tisenres <tisenres@gmail.com>
2024-03-05 21:55:07 +0500

    Add Gemini AI chat

--- app/src/main/java/com/example/englishguru/app/features/aichat/ChatViewModel.kt
+++ app/src/main/java/com/example/englishguru/app/features/aichat/ChatViewModel.kt
@@ -3,6 +3,8 @@ package com.example.englishguru.app.features.aichat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.asTextOrNull
+import com.google.ai.client.generativeai.type.content
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,24 +14,23 @@ class ChatViewModel(
     generativeModel: GenerativeModel
 ) : ViewModel() {
     private val chat = generativeModel.startChat(
-//        history = listOf(
-//            content(role = "user") { text("Hello, I have 2 dogs in my house.") },
-//            content(role = "model") { text("Great to meet you. What would you like to know?") }
-//        )
-    )
-
-    private val _uiState: MutableStateFlow<ChatUiState> = MutableStateFlow<ChatUiState>(
-        ChatUiState(
-            emptyList()
+        history = listOf(
+            content(role = "user") { text("Hello, I have 2 dogs in my house.") },
+            content(role = "model") { text("Great to meet you. What would you like to know?") }
         )
     )
-//        MutableStateFlow(ChatUiState(chat.history.map { content ->
-//            ChatMessage(
-//                text = content.parts.first().asTextOrNull() ?: "",
-//                participant = if (content.role == "user") Participant.USER else Participant.MODEL,
-//                isPending = false
-//            )
-//        }))
+
+    private val _uiState: MutableStateFlow<ChatUiState> =
+        MutableStateFlow(
+            ChatUiState(chat.history.map { content ->
+                ChatMessage(
+                    text = content.parts.first().asTextOrNull() ?: "",
+                    participant = if (content.role == "user") Participant.USER else Participant.MODEL,
+                    isPending = false
+                )
+            })
+        )
+
     val uiState: StateFlow<ChatUiState> =
         _uiState.asStateFlow()
 
